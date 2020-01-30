285936dae1220c0b730c2748682596aaebf5dfd4
Dennis Padiernos <depadiernos@gmail.com>
2020-01-29 23:02:18 -0800

    create knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,35 @@
+const sqlite = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  migrations: {
+    directory: "./data/migrations"
+  },
+  seeds: {
+    directory: "./data/seeds"
+  }
+}
+
+module.exports = {
+  development: {
+    ...sqlite,
+    connection: {
+      filename: "./data/development.db3"
+    }
+  },
+  test: {
+    ...sqlite,
+    connection: {
+      filename: "./data/test.db3"
+    }
+  },
+  production: {
+    client: "pg",
+    connection: process.env.PG_URL,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
+}
