fe78304333d8b5aa9a8014773729b2afb96279c2
Dennis Padiernos <depadiernos@gmail.com>
2020-01-29 23:59:57 -0800

    users db access file

---
+++ routes/users/users-model.js
@@ -0,0 +1,28 @@
+const bcrypt = require("bcryptjs")
+const db = require("../database/dbConfig")
+
+const find = (filter) => {
+  return db("users")
+    .where(filter)
+    .select("id", "username")
+}
+
+const findBy = (filter) => {
+  return db("users")
+    .where(filter)
+    .select(["id", "username", "password"]).first()
+}
+
+const findById = (id) => {
+  return db("users")
+    .where({ id })
+    .first("id", "username")
+}
+
+const add = async (user) => {
+  user.password = await bcrypt.hash(user.password, 12)
+  const [id] = await db("users").insert(user)
+  return findById(id)
+}
+
+module.exports = { find, findBy, findById, add }
