1cf21074475e01afb9d8985fba38113859b67a5e
Dennis Padiernos <depadiernos@gmail.com>
2020-01-30 21:33:08 -0800

    add logger

--- index.js
+++ index.js
@@ -11,18 +11,29 @@ const setsRouter = require("./routes/sets/sets-router")
 const server = express()
 const port = process.env.PORT || 4000
 
+const logger = () => (req, res, next) => {
+  console.log(`[${new Date().toISOString()}]: ${req.method} - ${req.url}`)
+  next()
+}
+
 server.use(cors())
 server.use(helmet())
 server.use(express.json())
+server.use(logger())
 
-server.use("/api/", usersRouter)
 server.use("/api/", authenticate, authRouter)
+server.use("/api/", usersRouter)
 server.use("/api/exercises", exercisesRouter)
 server.use("/api/sets", setsRouter)
 server.use("/api/regions", regionsRouter)
 
 server.get("/", (req, res) => {
   res.json({ message: "WeightLifting API" })
+})
+
+server.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).json({ message: "Something went wrong!" })
 })
 
 if (!module.parent) {
