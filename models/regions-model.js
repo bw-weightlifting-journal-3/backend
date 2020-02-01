19a58cfef208be9da70eaf5a7a2ee4136609b050
Dennis Padiernos <depadiernos@gmail.com>
2020-02-01 00:40:26 -0800

    implement regions model

---
+++ models/regions-model.js
@@ -0,0 +1,34 @@
+const db = require("../data/dbConfig")
+
+const find = () => {
+  return db("regions")
+}
+
+const findBy = (filter) => {
+  return db("regions")
+    .where(filter)
+    .first()
+}
+
+const findById = (id) => {
+  return db("regions")
+    .where({ id })
+    .first()
+}
+
+const add = async (region) => {
+  const [id] = await db("regions").insert(region, "id")
+  return findById(id)
+}
+
+const update = async (region, id) => {
+  await db("regions").where({id}).update(region)
+  return findById(id)
+}
+const remove = (id) => {
+  return db("regions")
+    .where({ id })
+    .del()
+}
+
+module.exports = { find, findBy, findById, add, update, remove }
