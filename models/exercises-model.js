b7bf4c1480423f449486817efb07af217abc7127
Dennis Padiernos <depadiernos@gmail.com>
2020-02-01 00:40:14 -0800

    implement nested sets

--- models/exercises-model.js
+++ models/exercises-model.js
@@ -10,20 +10,32 @@ const findBy = (filter) => {
     .first()
 }
 
-const findById = (id) => {
-  return db("exercises")
+const findById = async (id) => {
+  const exercise = await db("exercises")
     .where({ id })
     .first()
+  const sets = await db("sets")
+    .where("exercise_id", exercise.id)
+    .select("id", "reps", "weight")
+  return {...exercise, sets}
 }
 
 const add = async (exercise) => {
-  const [id] = await db("exercises").insert(exercise)
+  const [id] = await db("exercises").insert(exercise, "id")
   return findById(id)
 }
 
 const update = async (exercise, id) => {
-  await db("exercises").where({id}).update(exercise)
+  await db("exercises")
+    .where({ id })
+    .update(exercise)
   return findById(id)
 }
 
-module.exports = { find, findBy, findById, add, update }
+const remove = (id) => {
+  return db("exercises")
+    .where({ id })
+    .del()
+}
+
+module.exports = { find, findBy, findById, add, update, remove }
