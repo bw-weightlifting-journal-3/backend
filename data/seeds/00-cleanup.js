99780d38a280156d643b61702d319aef0ba3901c
Dennis Padiernos <depadiernos@gmail.com>
2020-01-31 23:40:22 -0800

    use raw query to truncate all tables at the same time.

--- data/seeds/00-cleanup.js
+++ data/seeds/00-cleanup.js
@@ -1,10 +1,5 @@
 
 exports.seed = async (knex) => {
-  await knex.raw('SET foreign_key_checks = 0');
-  await knex('sets').truncate()
-  await knex('exercises').truncate()
-  await knex('regions').truncate()
-  await knex('users').truncate()
-  await knex.raw('SET foreign_key_checks = 1');
+  await knex.raw('truncate users, regions, exercises, sets cascade')
 
 };
