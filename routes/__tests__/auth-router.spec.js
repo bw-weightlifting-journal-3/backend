17daa59318089a5a6957f14d62072db106b86688
Dennis Padiernos <depadiernos@gmail.com>
2020-02-04 22:25:06 -0600

    implement tests for auth endpoints

---
+++ routes/__tests__/auth-router.spec.js
@@ -0,0 +1,34 @@
+const request = require("supertest")
+const db = require("../../data/dbConfig")
+const server = require("../../index")
+
+beforeEach(async () => {
+  await db.seed.run()
+})
+
+describe("auth router", () => {
+  test("register a new user", async () => {
+    const res = await request(server)
+      .post("/api/register")
+      .send({ email: "me8@me.com", name: "me", password: "password" })
+    expect(res.status).toBe(201)
+  })
+  test("register a new user", async () => {
+    const res = await request(server)
+      .post("/api/register")
+      .send({ email: "me8@me.com", name: "me", password: "password" })
+    expect(res.body.email).toBe("me8@me.com")
+  })
+  test("login user with correct password", async () => {
+    const res = await request(server)
+      .post("/api/login")
+      .send({ email: "me@me.com", password: "password" })
+    expect(res.status).toBe(200)
+  })
+  test("login user with incorrect password", async () => {
+    const res = await request(server)
+      .post("/api/login")
+      .send({ email: "me@me.com", password: "password@" })
+    expect(res.status).toBe(401)
+  })
+})
