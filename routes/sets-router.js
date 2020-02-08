aec478dde2144f42312d85b6269f85d214cc14b1
Dennis Padiernos <depadiernos@gmail.com>
2020-02-07 20:23:53 -0600

    make sure sets endpoint work even if payload value is sent as string.

--- routes/sets-router.js
+++ routes/sets-router.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const setsModel = require("../models/sets-model")
 
-const router = express.Router({mergeParams: true})
+const router = express.Router({ mergeParams: true })
 
 router.get("/", async (req, res, next) => {
   try {
@@ -23,7 +23,8 @@ router.get("/:set_id", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
-    const set = await setsModel.add(req.body, req.params.id)
+    const newSet = { reps: parseInt(req.body.reps), weight: parseInt(req.body.weight) }
+    const set = await setsModel.add(newSet, req.params.id)
     res.status(201).json(set)
   } catch (err) {
     next(err)
@@ -43,7 +44,7 @@ router.delete("/:set_id", async (req, res, next) => {
   try {
     const set = await setsModel.findById(req.params.set_id, req.params.id)
     await setsModel.remove(req.params.set_id)
-    res.status(204).json({message: `set has been deleted`})
+    res.status(204).json({ message: `set has been deleted` })
   } catch (err) {
     next(err)
   }
